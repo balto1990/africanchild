557a80027a528b6c7274b4bf6e63acadc802671a
balto1990 <92270489+balto1990@users.noreply.github.com>
2022-05-01 19:45:48 +0200

    Add files via upload
    Google térkép betöltése egy adott oldalra

---
+++ aboutus.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>About Us | African Children's Fund</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php include("header.html"); ?>
+    <div id="wrapper">
+        <div id="content">
+            <h2>Contact:</h2>
+            <p>Organization name: <strong>African Children's Fund</strong></p>
+            <p>Address:</p>
+            <p><strong>Silver Birch House</strong></p>
+            <p><strong>Cow Lane</strong></p>
+            <p><strong>Longworth</strong></p>
+            <p><strong>Abingdon</strong></p>
+            <p><strong>OXON</strong></p>
+            <p><strong>OX13 5EJ</strong></p>
+            <p>Phone: <strong>+44 (0)1865 820570</strong></p>
+            <p>Email: <strong>info@africanchildrensfund.org</strong></p>
+            <iframe src="https://www.google.com/maps/place/African+Children's+Fund/@51.6891353,-1.4359826,17z/data=!3m1!4b1!4m5!3m4!1s0x4876c9f6803fc3b7:0x74c5d1ac8751f35c!8m2!3d51.6891353!4d-1.4337939" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <br>
+            <a target="_blank" href="https://www.google.com/maps/place/African+Children's+Fund/@51.6891353,-1.4359826,17z/data=!3m1!4b1!4m5!3m4!1s0x4876c9f6803fc3b7:0x74c5d1ac8751f35c!8m2!3d51.6891353!4d-1.4337939">Nagyobb térkép</a>
+        </div>
+    </div>
+    <?php include("footer.html"); ?>
+</body>
+</html>
