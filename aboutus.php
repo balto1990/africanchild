3698eb9b6a57328c9e27b6cdc4d7c99021ede113
balto1990 <92270489+balto1990@users.noreply.github.com>
2022-05-01 19:52:52 +0200

    Add files via upload
    Google térkép hozzáfűzése az egyik oldalhoz.

--- aboutus.php
+++ aboutus.php
@@ -20,7 +20,7 @@
             <p><strong>OX13 5EJ</strong></p>
             <p>Phone: <strong>+44 (0)1865 820570</strong></p>
             <p>Email: <strong>info@africanchildrensfund.org</strong></p>
-            <iframe src="https://www.google.com/maps/place/African+Children's+Fund/@51.6891353,-1.4359826,17z/data=!3m1!4b1!4m5!3m4!1s0x4876c9f6803fc3b7:0x74c5d1ac8751f35c!8m2!3d51.6891353!4d-1.4337939" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://g.page/african-childrens-fund-longworth" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             <br>
             <a target="_blank" href="https://www.google.com/maps/place/African+Children's+Fund/@51.6891353,-1.4359826,17z/data=!3m1!4b1!4m5!3m4!1s0x4876c9f6803fc3b7:0x74c5d1ac8751f35c!8m2!3d51.6891353!4d-1.4337939">Nagyobb térkép</a>
         </div>
